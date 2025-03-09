85d8b989f1162c99e066dab00f078d9d68b997d4
Impulse-Showdown <impulse.pokemonshowdown@gmail.com>
2025-03-09 22:03:14 +0530

    Update economy-config.ts

--- impulse/economy/economy-config.ts
+++ impulse/economy/economy-config.ts
@@ -1,7 +1,7 @@
 import * as nodePersist from 'node-persist';
 
 // Initialize node-persist storage for the economy system
-export const Economy = nodePersist.create({ dir: '../impulse/database/economy' });
+export const Economy = nodePersist.create({ dir: '../impulse/economy/database' });
 
 (async () => {
     await Economy.init();
